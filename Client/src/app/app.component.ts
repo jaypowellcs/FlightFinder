d6535369094cf256a6c9692671b6787fc4a7b1cd
Jay Powell <jay.powellcs@gmail.com>
2023-12-20 14:12:30 -0600

    Add Header, Footer, Home, Work on Header CSS+HTML

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Shared/header/header.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { FooterComponent } from './Shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Client';
+  title = 'FlightFinder';
 }
