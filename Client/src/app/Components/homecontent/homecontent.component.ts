dbd58c308b4227fc4ecd3a70746970c0508ef1a3
Jay Powell <jay.powellcs@gmail.com>
2023-12-27 22:04:01 -0600

    edited footer

---
+++ Client/src/app/Components/homecontent/homecontent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homecontent',
+  standalone: true,
+  imports: [],
+  templateUrl: './homecontent.component.html',
+  styleUrl: './homecontent.component.scss'
+})
+export class HomecontentComponent {
+
+}
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Shared/header/header.component';
-import { HomeComponent } from './Pages/home/home.component';
 import { FooterComponent } from './Shared/footer/footer.component';
+import { HomeComponent } from './Pages/home/home/home.component';
 import { LoginPageComponent } from './Pages/home/login-page/login-page.component';
 import { TripFinderComponent } from './Components/trip-finder/trip-finder.component';
+import { HomecontentComponent } from './Components/homecontent/homecontent.component';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,7 @@ import { TripFinderComponent } from './Components/trip-finder/trip-finder.compon
     FooterComponent,
     LoginPageComponent,
     TripFinderComponent, 
+    HomecontentComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
