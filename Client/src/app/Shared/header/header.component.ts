0132ce4cf92c3c1e9b75dfb62fc71b81c95eece4
Jay Powell <jay.powellcs@gmail.com>
2023-12-30 21:08:48 -0600

    fixed nav css/html

--- Client/src/app/Shared/header/header.component.ts
+++ Client/src/app/Shared/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgClass ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  // Initialization for ES Users
+  showMenu = false;
 
+  toggleNavbar(){
+    console.log("Clicked!");
+    this.showMenu = !this.showMenu;
+  }
 }
